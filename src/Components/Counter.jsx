7fd9c33126c1150abd11a154b3c138c4bb76e6e1
asp <asp@aptech.com>
2026-09-04 10:31:32 +0500

    Initial commit

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,33 @@
+
+import {useState} from "react";
+
+function Counter (){
+
+    const [count,setCount]= usState (0);
+     function handelAdd(){
+        setCount (count+1)
+     }
+     function handleSubtract(){
+        setCount(count-1)
+     }
+     function handleReset(){
+        setCount(0)
+     }
+
+
+
+return(
+<div>
+    <h2>Counter</h2>
+    <p>Current Count:{count} </p>
+    <button onClick={handelAdd}>Add 1</button>
+    <button onClick={handleSubtract}>Subtract 1</button>
+    <button onClick={handleReset}>Reset</button>
+</div>
+
+
+
+)
+}
+
+export default Counter;
